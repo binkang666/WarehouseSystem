705d9c2c44ec453b9c9f6e094724db838cafdcd6
Xronier <45444767+Xronier@users.noreply.github.com>
2020-11-25 00:36:01 -0800

    Add files via upload

---
+++ src/LoginBoundary.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class LoginBoundary {
+    // controller objects contain boundary objects?
+
+    // boundary is similar to view
+    LoginController loginController;
+
+    void showLoginUI() {
+        Scanner sc = new Scanner(System.in);
+        int input = 0;
+        String password;
+        Boolean valid;
+
+        while (input != 3) {
+            System.out.println("Select an option\n" +
+                    "1. Enter password\n" +
+                    "2. Change password\n" +
+                    "3. Exit");
+            input = sc.nextInt();
+            if (input == 1) {
+                System.out.println("Enter password: ");
+                password = sc.nextLine();
+                valid = loginController.login(password);
+                // check if password is valid, if it is print main ui.
+                showMainUI();
+            }
+            else if (input == 2) {
+                System.out.println("Enter password: ");
+                password = sc.nextLine();
+                valid = loginController.login(password);
+                // if password valid
+                System.out.println("Enter new password: ");
+                password = sc.nextLine();
+                loginController.setPassword(password);
+                System.out.println("Your password has been updated.");
+            }
+            else {
+                input = 3;
+            }
+        }
+    }
+
+    void showMainUI() {
+
+    }
+}
---
+++ src/LoginController.java
@@ -0,0 +1,12 @@
+public class LoginController {
+    User user;
+
+    public Boolean login(String password) {
+        // check txt for password and return result
+    }
+
+    public void setPassword(String password) {
+        // replace password in txt
+        user.password = password;
+    }
+}
