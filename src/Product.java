4b548bf3d26f0547b5877920cda2cdaf50d17a64
mhy9910 <74801064+mhy9910@users.noreply.github.com>
2020-12-01 23:23:19 -0800

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,6 +1,8 @@
 public class Product {
     private String productName;
     private double costPrice, sellingPrice;
+    
+    private String storedIn;
 
     private int quantitySold;
     private int quantity;
@@ -8,10 +10,26 @@ public class Product {
     private double totalProfit = (sellingPrice * quantitySold) - (costPrice * quantitySold);
     private double totalProfitPercent = (totalProfit / (sellingPrice * quantitySold)) * 100;
     private double totalSales = quantitySold * sellingPrice;
+    
+    //showAllProducts
+    
+    //showProductsLessThan5
+    
 
-
+    //getter
     public double getSellingPrice() {
         return sellingPrice;
     }
+    public double getcostPrice() {
+        return costPrice;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    
+    
+    
+    
+    
 }
 
