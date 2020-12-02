02aba2b4426c63934e734121f74536a859f61ec3
mhy9910 <74801064+mhy9910@users.noreply.github.com>
2020-12-01 23:43:55 -0800

    Update Warehouse.java

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,3 +1,14 @@
 public class Warehouse {
+  private String name;
+  private String address;
+  private String city;
+  private String state;
+  private String zip;
+  private String phoneNumber;
+  
+  private ArrayList<Product> productList;
+  
+  
+  
   
 }
