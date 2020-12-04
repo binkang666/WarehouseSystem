d44c0625d525cc583170836ca5e870cc40f1b279
binkang666 <54644269+binkang666@users.noreply.github.com>
2020-12-03 19:11:17 -0800

    Create CustomerBoundary.java

---
+++ src/CustomerBoundary.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class CustomerBoundary {
+    CustomerController customerController;
+
+    public CustomerBoundary(CustomerController customerController){
+        this.customerController = customerController;
+    }
+
+    void showCustomerUI(){
+
+        Scanner input = new Scanner(System.in);
+        sop("""
+                            1. add customers
+                            2. Show all customers
+                            3. Change customer status
+                            4. Return to main menu"""
+        );
+        int choice_customer = input.nextInt();
+        switch (choice_customer){
+            case 1 -> {
+                sop("\n*********************************************");
+                sop("adding customers");
+                Customer c = new Customer();
+
+                sop("Enter first name for new customer");
+                input.nextLine();
+                String fn = input.nextLine();
+                c.setFirst_name(fn);
+
+                sop("Enter last name for new customer");
+                String ln = input.nextLine();
+                c.setLast_name(ln);
+
+                sop("Enter phone for new customer");
+                String phone = input.nextLine();
+                c.setPhone(phone);
+
+                sop("Enter address for new customer");
+                String add = input.nextLine();
+                c.setAddress(add);
+
+                sop("Enter salesTax for new customer");
+                double tax = input.nextDouble();
+                c.setSalesTax(tax);
+
+                Main.writeCustomer(c);
+                sop("New Customers added");
+
+
+            }  //OK
+            case 2 -> {sop("showing all customers");
+                Main.displayAllCustomers();
+
+            }  //OK
+            case 3 -> {sop("Enter the customer ID for the customer who you want change status");
+
+
+            }  //Need implementation, prob gonna delete this
+            case 4 -> {Main.showMainUI();} //OK
+            default -> sop("Please choose number from 1 - 3");
+        }
+    }
+
+
+    private static void sop(String s){
+        System.out.println(s);
+    }
+}
