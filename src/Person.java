28242db4468fafb7090a026490086db4eef1450b
binkang666 <54644269+binkang666@users.noreply.github.com>
2020-12-03 19:15:33 -0800

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,5 @@
-import java.util.*;
 
-public abstract class Person  {
+public abstract class Person {
     private String first_name;
     private String last_name;
     private String phone;
@@ -26,24 +25,15 @@ public abstract class Person  {
         return "Name: " + getName() + ", Address: " + getAddress() + ", Phone: " + getPhone();
     }
 
-    public String display(){
-        return toString() + "\n";
-    }
-
     public abstract String getType();
-
-    //getter - Boundary
-    public String getFirst_name() { return first_name; }
-    public String getLast_name() { return last_name; }
+    
     public String getPhone() { return phone; }
-    public String getName() { return name = first_name + " " + last_name; }
+    public String getName() { return name = first_name +" " + last_name; }
     public String getAddress() { return address; }
-
-    //setter - Controller
+    
     public void setFirst_name(String first_name) { this.first_name = first_name; }
     public void setLast_name(String last_name) { this.last_name = last_name; }
     public void setPhone(String phone) { this.phone = phone; }
-    public void setName(String name) { this.name = name; }
     public void setAddress(String address) { this.address = address; }
 
 }
