cdde954804eaac05197be5437f443a2249894c0a
binkang666 <54644269+binkang666@users.noreply.github.com>
2020-12-04 20:50:56 -0800

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -8,14 +8,14 @@ import java.util.List;
 public class Customer extends Person implements Comparable<Customer> {
 
     private static int customerID = 0;
-    private int status;
+    private boolean status;
     private ArrayList<Invoice> InvoiceAssociated;
     private double salesTax;
     private LocalDate lastOrderDate;
 
     public Customer(){
         customerID++;
-        status= 1;
+        status= true;
         lastOrderDate = null;
     }
 
@@ -23,7 +23,7 @@ public class Customer extends Person implements Comparable<Customer> {
     public Customer(String firstName, String lastName, String phone, String address, double salesTax) {
         super(firstName, lastName, phone, address);
         customerID++;
-        status = 1;
+        status = true;
         this.salesTax = salesTax;
         lastOrderDate = null;
     }
@@ -38,31 +38,31 @@ public class Customer extends Person implements Comparable<Customer> {
         }
     }
 
-    public int isActive(){
+    public boolean isActive(){
         if(lastOrderDate ==null){
-            return status = 1; //active
+            return status = true; //active
         }
         else {
             LocalDate today = LocalDate.now();
             long p = ChronoUnit.DAYS.between(lastOrderDate, today);
             if(p < 365){
-                return status = 1; //active
+                return status = true; //active
             }
             else
-                return status = 0; //inactive, last order more than 365 days
+                return status = false; //inactive, last order more than 365 days
         }
 
     }
     public void changeStatus() {
-        if(status == 0){
-            status = 1;
+        if(status = false){
+            status = true;
             System.out.println("the customer is now marked active");}
-        else if (status == 1){
-            status = 0;
+        else if (status = true){
+            status = false;
             System.out.println("the customer is now marked inactive");}
     }
     public int getCustomerID() { return Customer.customerID; }
-    public int getStatus() { return status; }
+    public boolean getStatus() { return status; }
     public double getSalesTax() { return salesTax; }
     public void setSalesTax(double salesTax) { this.salesTax = salesTax; }
 
@@ -74,7 +74,7 @@ public class Customer extends Person implements Comparable<Customer> {
     @Override
     public String toString()
     {
-        return ", Type: "+ getType() + ", " + super.toString() +
+        return " (Customer ID), Type: "+ getType() + ", " + super.toString() +
                 ", Status: " + isActive()  + ", Sales Tax: " + getSalesTax();
     }
 
