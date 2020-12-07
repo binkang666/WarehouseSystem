a9b63e319fadf7a3a7a8e5e52f873d9e6fccbb11
binkang666 <54644269+binkang666@users.noreply.github.com>
2020-12-06 18:56:09 -0800

    Update Salesperson.java

--- src/Salesperson.java
+++ src/Salesperson.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 
 public class Salesperson extends Person implements Comparable<Salesperson> {
-    private static int salespersonID = 0;
+    private int salespersonID;
     private double totalCommissionEarned;
     private double totalSales;
     private LocalDate startDate;
@@ -10,21 +10,22 @@ public class Salesperson extends Person implements Comparable<Salesperson> {
     public Salesperson(){
         this.totalCommissionEarned = 0;
         this.totalSales = 0;
-        this.startDate = LocalDate.of(2020,12,11);
+        this.startDate = LocalDate.now();
         this.commissionRate = 0;
-        salespersonID++;
+        this.salespersonID = 0;
 
     }
 
 
     public Salesperson(String firstName, String lastName, String phone, String address,
-                       double totalCommissionEarned, double totalSales, LocalDate startDate, double commissionRate) {
+                       double totalCommissionEarned, double totalSales, LocalDate startDate,
+                       double commissionRate, int salespersonID) {
         super(firstName, lastName, phone, address);
         this.totalCommissionEarned =totalCommissionEarned;
         this.totalSales = totalSales;
         this.startDate = startDate;
         this.commissionRate = commissionRate;
-        salespersonID++;
+        this.salespersonID = salespersonID;
     }
     @Override
     public String getType() {
@@ -33,15 +34,10 @@ public class Salesperson extends Person implements Comparable<Salesperson> {
     @Override
     public String toString()
     {
-        return " (Salesperson ID), Type: "+ getType() +" "+ super.toString() +  ", Start Date: " + getStartDate() +
-                ", Commission Rate: " + getCommissionRate() + ", Commission Earned: "+ getTotalCommissionEarned()
+        return "Salesperson ID: " +getSalespersonID() +", Type: " + getType() +", "+ super.toString() +  ", Start Date: " + getStartDate() +
+                ", Commission Rate: " + getCommissionRate() + "%" + ", Commission Earned: "+ getTotalCommissionEarned()
                 +", Total sales: " + getTotalSales();
     }
-
-  /*  public String showAllPerformances(){
-        return " (Salesperson ID), " + super.getName() +", Commission Earned: "+ getTotalCommissionEarned() +
-                ", Total sales: " + getTotalSales();
-    }*/
 
     //getter - Boundary
     public double getTotalCommissionEarned() { return totalCommissionEarned; }
