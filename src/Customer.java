356c63a0bf1f25076087b477e0982ebd4bd73527
binkang666 <54644269+binkang666@users.noreply.github.com>
2020-12-10 19:09:45 -0800

    Formatting the codes and Removing the unnecessary method

--- src/Customer.java
+++ src/Customer.java
@@ -9,13 +9,6 @@ public class Customer extends Person implements Comparable<Customer>, Serializab
     private boolean status;
     private HashMap<Integer, Invoice> InvoiceAssociated;
     private BigDecimal salesTax;
-    private LocalDate lastOrderDate;
-
-//    public Customer(){
-//        this.customerID = 0;
-//        status= true;
-//        lastOrderDate = null;
-//    }
 
 
     public Customer(String firstName, String lastName, String phone, String address, BigDecimal salesTax, int customerID) {
@@ -23,72 +16,19 @@ public class Customer extends Person implements Comparable<Customer>, Serializab
         this.customerID = customerID;
         status = true;
         this.salesTax = salesTax;
-        lastOrderDate = null;
         InvoiceAssociated = new HashMap<>();
-    }
-
-    public void addInvoiceAssociated(Invoice ia){
-        InvoiceAssociated.put(ia.getInvoiceNumber(), ia);
     }
 
     public HashMap<Integer, Invoice> getInvoiceAssociated(){
         return InvoiceAssociated;
     }
 
-
-    public void displayInvoiceAssociated() {
-        for(Invoice invoice: InvoiceAssociated.values()){
-            System.out.println(invoice);
-        }
-    }
-
-    public boolean isActive(){
-        if(lastOrderDate == null){
-            return status = true; //active
-        }
-        else {
-            LocalDate today = LocalDate.now();
-            long p = ChronoUnit.DAYS.between(lastOrderDate, today);
-            if(p < 365){
-                return status = true; //active
-            }
-            else
-                return status = false; //inactive, last order more than 365 days
-        }
-
-    }
-    public void changeStatus() {
-        if(status = false){
-            status = true;
-            System.out.println("the customer is now marked active");}
-        else if (status = true){
-            status = false;
-            System.out.println("the customer is now marked inactive");}
-    }
     public int getCustomerID() { return customerID; }
     public boolean getStatus() { return status; }
     public BigDecimal getSalesTax() { return salesTax; }
-    public void setSalesTax(BigDecimal salesTax) { this.salesTax = salesTax; }
-
-
-    public boolean isStatus() {
-        return status;
-    }
 
     public void setStatus(boolean status) {
         this.status = status;
-    }
-
-    public void setInvoiceAssociated(HashMap<Integer, Invoice> invoiceAssociated) {
-        InvoiceAssociated = invoiceAssociated;
-    }
-
-    public LocalDate getLastOrderDate() {
-        return lastOrderDate;
-    }
-
-    public void setLastOrderDate(LocalDate lastOrderDate) {
-        this.lastOrderDate = lastOrderDate;
     }
 
     @Override
